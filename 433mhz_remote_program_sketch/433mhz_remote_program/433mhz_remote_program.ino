53339d4f736a970eff7568a955d3f7919b2180e6
James Wakefield <jmw1982@gmail.com>
2023-04-07 19:19:58 +1000

    garage remote testing notes, sketch to program them using transmitter

---
+++ 433mhz_remote_program_sketch/433mhz_remote_program/433mhz_remote_program.ino
@@ -0,0 +1,45 @@
+#include <RCSwitch.h>
+
+RCSwitch tx = RCSwitch();
+
+void setup() {
+  
+  Serial.begin(9600);
+  
+  // digital I/O pin 8
+  tx.enableTransmit(8);
+
+  // these options from receive logs of remotes
+  tx.setProtocol(1);
+  tx.setPulseLength(340);
+
+  // not sure now many repetitions we really need
+  tx.setRepeatTransmit(1000);
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  // codes are 24 bits
+
+  // first 12 bits identifies the ref, 
+  // next 12 bits identifies the button
+
+  // head ref clear button
+  //tx.send("000000000001000000000001");
+  // head ref red button
+  //tx.send("000000000001000000000010");
+  // head ref white button
+  //tx.send("000000000001000000000100");
+  // left ref red button
+  //tx.send("000000000010000000000010");
+  // left ref white button
+  //tx.send("000000000010000000000100");
+  // right ref red button
+  //tx.send("000000000100000000000010");
+  // right ref white button
+  tx.send("000000000100000000000100");
+
+}
